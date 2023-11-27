4f3cfcff4a1d5c332abdece59c892f3faf2ecf31
Matt Karmazyn <matt@itskarma.wtf>
2023-11-27 08:21:20 -0500

    updates

--- README.md
+++ README.md
@@ -4,12 +4,27 @@ ChatGPT over your swagger docs
 
 ## Usage
 
+### Setup
+
+To avoid having to download/install pip packages locally and clutter your filesystem, you can run the following docker command to open a temporary scratch space. You can put all of your environment/secrets in a .env file that will be loaded.
+
+```
+docker run -it --rm --env-file .env -v "$(pwd):/app" -v "$HOME/.aws:/root/.aws" -w "/app/" python:3-slim-buster /bin/bash
+
+# upgrade pip
+pip install --upgrade pip
+
+# install requirements
+pip install -r requirements.txt
+```
+
 ### chatgpt.py
 
 This script will load the files from the `exported/` directory, take your prompt, and use GPT to provide you with a response over your custom data.
 
 ```
 export OPENAI_API_KEY='YOUR_API_KEY'
+export OPENAI_ORG='YOUR_ORG_ID'
 
 python3 chatgpt.py "This is my question over my data"
 ```
@@ -25,4 +40,17 @@ export ATLASSIAN_URL='https://YOUR_WORKSPACE.atlassian.net'
 export ATLASSIAN_SPACE_TO_EXPORT='YOUR_SPACE'
 
 python3 export_confluence.py
+```
+
+### export_s3.py
+
+We keep swagger docs in an s3 bucket, so this script simply exports the files from the s3 bucket to the `exported/` directory. You will first need to create your cli connection to your aws account first.
+
+```
+aws sso login --sso-session YOURSESSIONNAME
+
+export S3_BUCKET_NAME='YOURBUCKETNAME'
+export AWS_PROFILE='YOURPROFILENAME'
+
+python3 export_s3.py
 ```
--- chatgpt.py
+++ chatgpt.py
@@ -1,3 +1,4 @@
+__import__('pysqlite3')
 import os
 import sys
 
@@ -6,23 +7,34 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.chat_models import ChatOpenAI
 
 KEY = os.getenv('OPENAI_API_KEY')
+ORG_ID = os.getenv('OPENAI_ORG_ID')
+EXPORTED_DIR = "./exported"
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 query = sys.argv[1]
-print(query)
 
 txt_loader = DirectoryLoader(
-    "./exported", glob="**/*.txt", show_progress=True, loader_cls=TextLoader)
+    EXPORTED_DIR, glob="**/*.txt", show_progress=True, loader_cls=TextLoader)
 json_loader = DirectoryLoader(
-    "./exported", glob="**/*.json", show_progress=True, loader_cls=TextLoader)
+    EXPORTED_DIR, glob="**/*.json", show_progress=True, loader_cls=TextLoader)
 csv_loader = DirectoryLoader(
-    "./exported", glob="**/*.csv", show_progress=True, loader_cls=TextLoader)
+    EXPORTED_DIR, glob="**/*.csv", show_progress=True, loader_cls=TextLoader)
 xml_loader = DirectoryLoader(
-    "./exported", glob="**/*.xml", show_progress=True, loader_cls=TextLoader)
+    EXPORTED_DIR, glob="**/*.xml", show_progress=True, loader_cls=TextLoader)
+python_loader = DirectoryLoader(
+    EXPORTED_DIR, glob="**/*.py", show_progress=True, loader_cls=TextLoader)
+javascript_loader = DirectoryLoader(
+    EXPORTED_DIR, glob="**/*.js", show_progress=True, loader_cls=TextLoader)
+terraform_loader = DirectoryLoader(
+    EXPORTED_DIR, glob="**/*.tf", show_progress=True, loader_cls=TextLoader)
 pdf_loader = DirectoryLoader(
-    "./exported", glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
+    EXPORTED_DIR, glob="**/*.pdf", show_progress=True, loader_cls=PyPDFLoader)
 
 
 index = VectorstoreIndexCreator().from_loaders(
-    [txt_loader, json_loader, csv_loader, xml_loader, pdf_loader])
+    [txt_loader, json_loader, csv_loader, xml_loader, python_loader, javascript_loader, terraform_loader, pdf_loader])
 
-print(index.query(query, llm=ChatOpenAI()))
+print() # Just creating a blank line for readability
+print(index.query(query, llm=ChatOpenAI(openai_organization=ORG_ID, openai_api_key=KEY)))
